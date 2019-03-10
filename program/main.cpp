4574a4b9ae62d5357a9502b69cf2da499fbbc96c
nektoncik <nik-1905@mail.ru>
2019-03-10 23:05:37 +0300

    Added ability to select permissions for android applications

--- program/main.cpp
+++ program/main.cpp
@@ -1,15 +1,108 @@
 #include <gtkmm.h>
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
+#include <string>
 
-void on_button_clicked ()
-{
-    std::cout << "Hello World" << std::endl;
+using namespace std;
+
+bool internet = true;
+bool local_files = true;
+bool camera = true;
+bool geodata = true;
+
+void on_button_clicked (){
+
+    cout << "Hello World" << std::endl;
 	system("make -C .. apk");
 }
 
-int main(int argc, char *argv[])
-{
+void on_check1_toggled (){
+	internet = !internet;
+	cout<<internet<<endl;
+}
+
+void on_check2_toggled (){
+	local_files = !local_files;
+	cout<<local_files<<endl;
+}
+
+void on_check3_toggled (){
+	camera = !camera;
+	cout<<camera<<endl;
+}
+
+void on_check4_toggled (){
+	 geodata= !geodata;
+	 cout<<geodata<<endl;
+}
+
+void buildAndroidManifest (){
+	string line;
+	ifstream file("/home/nick/Документы/web_to_apk/app/AndroidManifestStart.xml");
+	if (file.is_open()){
+        while (getline(file, line)){
+            cout << line << endl;
+        }
+    }
+    file.close();
+
+	if (internet == true){
+		ifstream p1file("/home/nick/Документы/web_to_apk/app/AndroidManifestPermission1.xml");
+		if (p1file.is_open()){
+        	while (getline(p1file, line)){
+            	cout << line << endl;
+        	}
+    	}
+		p1file.close();
+	}
+
+	if (local_files == true){
+		ifstream p2file("/home/nick/Документы/web_to_apk/app/AndroidManifestPermission2.xml");
+		if (p2file.is_open()){
+        	while (getline(p2file, line)){
+            	cout << line << endl;
+        	}
+    	}
+		p2file.close();
+	}
+
+	if (camera == true){
+		ifstream p3file("/home/nick/Документы/web_to_apk/app/AndroidManifestPermission3.xml");
+		if (p3file.is_open()){
+        	while (getline(p3file, line)){
+            	cout << line << endl;
+        	}
+    	}
+		p3file.close();
+	}
+
+	if (geodata== true){
+		ifstream p4file("/home/nick/Документы/web_to_apk/app/AndroidManifestPermission4.xml");
+		if (p4file.is_open()){
+        	while (getline(p4file, line)){
+            	cout << line << endl;
+        	}
+    	}
+		p4file.close();
+	}
+
+
+	ifstream lastFile ("/home/nick/Документы/web_to_apk/app/AndroidManifestEnd.xml");
+		if (lastFile.is_open()){
+        	while (getline(lastFile, line)){
+            	cout << line << endl;
+        	}
+    	}
+    lastFile.close();
+
+}
+
+
+int main(int argc, char *argv[]){
+
+	buildAndroidManifest();
+
 	auto app = Gtk::Application::create(argc, argv, "org.gtkmm.examples.base");
 	auto builder = Gtk::Builder::create_from_file("app.glade");
 	Gtk::Window *window = nullptr;
@@ -18,6 +111,22 @@ int main(int argc, char *argv[])
 	Gtk::Button *button = nullptr;
 	builder->get_widget("button", button);
 	button->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
+
+	Gtk::CheckButton *check1 = nullptr;
+	builder->get_widget("check1", check1);
+	check1->signal_toggled().connect(sigc::ptr_fun(on_check1_toggled));
+	
+	Gtk::CheckButton *check2 = nullptr;
+	builder->get_widget("check2", check2);
+	check2->signal_toggled().connect(sigc::ptr_fun(on_check2_toggled));
+	
+	Gtk::CheckButton *check3 = nullptr;
+	builder->get_widget("check3", check3);
+	check3->signal_toggled().connect(sigc::ptr_fun(on_check3_toggled));
+	
+	Gtk::CheckButton *check4 = nullptr;
+	builder->get_widget("check4", check4);
+	check4->signal_toggled().connect(sigc::ptr_fun(on_check4_toggled));
 
 	app->run(*window);
 }
